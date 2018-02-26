2b04559af61f7e1ac11c0e3d31cc2ee4c7de3733
vishruty <vishruty@gmail.com>
2018-02-26 17:40:37 +0530

    search working

---
+++ WebContent/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+	<link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<form method="get">
+	<input type="text" name="movieName" id="movie_name">
+	<input type="button" name="nt" value="search" onClick="myFunction()">
+	</form>
+	<p id="output"></p>
+	 
+	<script src="main.js"></script>
+</body>
+</html>
---
+++ WebContent/main.js
@@ -0,0 +1,47 @@
+function myFunction(){
+var APIKEY = "1f39dea1e4239caa8c1ae2c4975e9926";
+var baseURL="https://api.themoviedb.org/3/";
+let url="".concat(baseURL,'configuration?api_key=',APIKEY);
+fetch(url)
+.then((result)=>{
+	return result.json();
+})
+.then((data)=>{
+	console.log('config:',data);
+	 var x = document.getElementById("movie_name").value; 
+	runSearch(x);
+})
+} 
+
+let runSearch=function(keyword){
+	let url=''.concat('https://api.themoviedb.org/3/','search/movie?api_key=','1f39dea1e4239caa8c1ae2c4975e9926','&query=',keyword);
+	fetch(url)
+	.then(result=>result.json())
+	.then((data)=>{
+		//document.getElementById('output').innerHTML=JSON.stringify(data,null,2);
+		//console.log(data.results.length);
+		document.getElementById('output').innerHTML=innerCode(data);
+		
+	})
+}
+
+let innerCode=function createData(data){
+	var len=data.results.length;
+	var movieObj="";
+	for(var i=0;i<len;i++){
+		movieObj+="<p class='nested_para'>";
+		movieObj+="Title:" + data.results[i].title + "<br>";
+		movieObj+= "Release date: " + data.results[i].release_date + "<br>";
+		movieObj+= "Rating: " + data.results[i].vote_average + "<br>";
+		if(data.results[i].overview.length!=0){
+			movieObj+= "Overview: " + data.results[i].overview + "<br>";
+		}
+		movieObj+= "<form>";
+		movieObj+="<input type='checkbox' name='vehicle' value='" + data.results[i].id + "'><br>";
+		movieObj+= "</form>"
+		movieObj+="</p>";
+		movieObj+= "<hr>";
+	}
+	return movieObj;
+}
+
